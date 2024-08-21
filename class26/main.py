edb2a1219e71368d356c0f35e71dcbb2bb566070
Ayyadurai <kmayyadurai286@gmailc.om>
2024-08-21 21:04:00 +0530

    class 26 finished

---
+++ class26/main.py
@@ -0,0 +1,23 @@
+#INHERITANCE
+
+class StudentProfile: # PARENT OR SUPER
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def display_info(self):
+        print("Name : ", self.name)
+        print("Age : ", self.age)
+
+class StudentMarks(StudentProfile): #CHILD OR SUB
+    def __init__(self, name, age, marks):
+        super().__init__(name, age) # CALL PARENT CLASS __INIT__ METHOD
+        self.marks = marks
+
+    def display_marks(self):
+        print("Marks : ", self.marks)
+
+obj = StudentMarks("Yesvnath" , 17 , "17,18,20")
+
+obj.display_info()
+obj.display_marks()
